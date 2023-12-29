2ef9265388fc04962390bf5d4802654baabb8b50
seeqgold <seeqrothgold@gmail.com>
2023-12-29 06:55:53 +0100

    images and styles were updated

---
+++ first-application/contact.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+const fullName = document.getElementById("fullname");
+const lastName = document.getElementById("lastname");
+const message = document.getElementById("message");
+form.addEventListener("submit", e ==>{
+    
+})
